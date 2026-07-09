6c9c93f3b41bba062cbf05fbab977c021703e5d7
tanvikohale <tanvikohale23@gmail.com>
2026-07-09 13:12:50 +0530

    Some Updates

--- JavaScript-Concepts/For-loop.js
+++
@@ -1,7 +0,0 @@
-function print_for_loop(){
-    for(let i=1; i<=10; i++){
-        console.log(i);
-    }
-}
-print_for_loop();
- 
--- JavaScript-Concepts/While-loop.js
+++
@@ -1,9 +0,0 @@
-function while_loop(){
-    let i=1;
-    while(i<=10){
-        console.log(i);
-        i++;
-    }
-}
-
-while_loop();
